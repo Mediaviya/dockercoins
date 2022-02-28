8eb095cdb4e761f984ab42f93a0442a0f617d813
Mediaviya <35331832+Mediaviya@users.noreply.github.com>
2022-02-28 12:32:24 +0100

    Update deploy-cloud9.sh

--- deploy-cloud9.sh
+++ deploy-cloud9.sh
@@ -11,3 +11,27 @@ FINAL
 ruby ./hasher/hasher.rb &
 
 # Instrucciones desplegar Redis
+sudo yum install -y redis
+
+sudo tee -a /etc/hosts 0<<FINAL
+#redis
+127.0.0.1 redis
+FINAL
+
+sudo systemctl start redis.service
+
+# Instrucciones desplegar RNG
+pip install Flask
+
+sudo tee -a /etc/hosts 0<<FINAL
+#rng
+127.0.0.1 rng
+FINAL
+
+python ./rng/rng.py &
+
+#Instrucciones para desplegar Worker
+pip install redis request
+
+python ./worker/worker.py &
+
